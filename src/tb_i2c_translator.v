1f199d0d46fa29ad31b9c7ca0fd0668b33577795
Chaitanya Sabnis <137556637+Chaitya5225@users.noreply.github.com>
2026-05-09 15:41:01 +0530

    Update tb_i2c_translator.v

--- src/tb_i2c_translator.v
+++ src/tb_i2c_translator.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.05.2026 22:58:54
-// Design Name: 
-// Module Name: tb_i2c_translator
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_i2c_translator();
 
